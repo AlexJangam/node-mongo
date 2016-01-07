79c387d6ab0a8328473f8714ec3966b825b536da
Alexander Jangam <alexander.jangam@wenable.com>
2016-01-07 13:18:54 +0530

    plugin stable1

--- plugin/mongoPlugin.js
+++ plugin/mongoPlugin.js
@@ -3,7 +3,7 @@
  * wt-node-mongo v1.0.0 (http://www.wenable.com/)
  * Copyright Wenable Technologies pvt Ltd 2016 , Inc.
  * Licensed under MIT (http://opensource.org/licenses/MIT)
-
+ * Desined for MongoDB v 3.2
 */
 
 
@@ -19,9 +19,9 @@ mongoPg = function(dbName,path,callback){
 	var connUrl = defaultPath+"/"+dbName,connDB,defColName;
 	if(typeof path ==='function')callback = path;
 	else if(typeof path ==='string')connUrl = path;
-	
+
 	function operationDB(opfn){
-		console.log("connUrl",connUrl)
+		//console.log("connUrl",connUrl)
 		MongoClient.connect(connUrl, function(err, mdb) {
 		if(err)console.log("err",err)
 		opfn(mdb);
@@ -34,7 +34,7 @@ mongoPg = function(dbName,path,callback){
 		if(typeof callB === "function")callB(err,data)
 		db.close();
 	}
-	
+
 	var createCollection = function(name,postCreate){
 		defColName = name;
 		if(!postCreate)postCreate = function(){}
@@ -43,13 +43,13 @@ mongoPg = function(dbName,path,callback){
 				console.log("creating",name)
 				if(err)console.log("Error",err)
 				if(colList.indexOf(name)==-1)colList.push(name)
-				
+
 				exeCallBack(err,con,postCreate,db);
 			});
 			//db.close();
 		})
 	}
-	
+
 	// set a default colection name to work with, and its latest created collection if not mentioned.
 	function setDefaultCollection(nm){
 		defColName = nm;
@@ -66,19 +66,19 @@ mongoPg = function(dbName,path,callback){
 			  }
 			  exeCallBack(err,colList,callB,db);
 			});
-			
+
 		  })
 	}
-	
-	
+
+
 	function insertOps(colcName,data,callB,many){
 		if(colList.indexOf(colcName)==-1){
 			colList.push(colcName)
 			console.log("Add new Collection");
 		}
-		// function for data insertion 
+		// function for data insertion
 		if(typeof colcName =="object" && defColName!= undefined){
-			//setting data and callB in case we used default collections name, where first element will be data 
+			//setting data and callB in case we used default collections name, where first element will be data
 			data = colcName;colcName = defColName;
 			if(typeof data ==='function')callB = data;
 		}
@@ -86,7 +86,7 @@ mongoPg = function(dbName,path,callback){
 			if(many){
 				db.collection(colcName).insertMany(data,function(err,res){
 				exeCallBack(err,res,callB,db)
-				})				
+				})
 			}else {
 				db.collection(colcName).insertOne(data,function(err,res){
 				console.log("err",err)
@@ -96,21 +96,62 @@ mongoPg = function(dbName,path,callback){
 
 		})
 	}
-	var colList = []
+	colList = []
 	getCollections()
-	
+
 	// Add an entry to DB
 	var addToCollection = function(colcName,data,callB){
 		insertOps(colcName,data,callB,false);
 	}
-	
+
 	// Add a set of objects to DB
 	var addBulkToCollection = function(colcName,data,callB){
 		insertOps(colcName,data,callB,true)
 	}
-	
 
-	
+	//Update Records
+	var updateRecords = function(colcName,findData,upData,callB,firstOnly){
+		if(typeof colcName =="object" && defColName!= undefined){
+			findData = colcName;upData = findData;colcName = defColName;
+			if(typeof upData ==='function')callB = data;
+		}
+		operationDB(function(db){
+			if(firstOnly === true){
+				db.collection(colcName).updateOne(findData,
+					{$set :upData},function(err,result){
+				exeCallBack(err,result,callB,db)});
+			}else {
+				db.collection(colcName).updateMany(findData,{$set :upData},function(err,result){
+				exeCallBack(err,result,callB,db)});
+			}
+		})
+	}
+
+	//Deleting records from collection
+	var removeRecords = function(colcName,findData,count,callB){
+		if(typeof colcName =="object" && defColName!= undefined){
+			findData = colcName;count = findData;colcName = defColName;
+		}
+		else if(typeof count ==='function'){callB = count;count=0}
+		count = count*1;
+		console.log(colcName,findData,count)
+		if(typeof findData !="object"){
+			throw "invalid format to remove data";
+		}else{
+			operationDB(function(db){
+				if(count != 0){
+					db.collection(colcName).remove(findData,count,function(err,result){
+					exeCallBack(undefined,result,callB,db)
+				});
+				}else {
+					db.collection(colcName).remove(findData,function(err,result){
+					exeCallBack(err,result,callB,db)});
+				}
+			})
+		}
+	}
+
+
 	//Default search Queries
 	var searchResult = function(colcName,data,callB){
 		var retRes = [];
@@ -118,36 +159,38 @@ mongoPg = function(dbName,path,callback){
 			data = colcName;colcName = defColName;
 			if(typeof data ==='function')callB = data;
 		}
+
 		operationDB(function(db){
-			var cursor = db.collection(colcName).find(data);
-			  cursor.each(function(err, doc) {
-			  if (doc != null) {
-				 console.dir(doc);
-				 retRes.push(doc);
-			  } else {
-				 console.error("no document");
-			  }
-			})
-			exeCallBack(undefined,res,callB,db)
+			var cursor =db.collection('trail').find(data);
+			console.log(cursor)
+	    cursor.each(function(err, doc) {
+	       if (doc != null) {
+						retRes.push(doc)
+	       } else {
+	        	exeCallBack(undefined,retRes,callB,db)
+	       }
+	    });
 		})
 	}
-	
+
 	var customSearch = {
 		searchById : function(){}
 	}
-	
+
+
+
 	return {
 		setCollection : setDefaultCollection,
 		getCollections : getCollections,
 		newCollection : createCollection,
 		add : addToCollection,
 		addBulk : addBulkToCollection,
+		remove : removeRecords,
 		search : searchResult,
 	}
-	
-	
+
+
 }
 
 
 module.exports = mongoPg;
-
